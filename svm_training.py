9ea65baa25d370344fb93228f421f4652b5d15ae
Rushabh <rush00121@gmail.com>
2017-01-31 20:01:30 -0800

    updating results - looks better output

--- svm_training.py
+++ svm_training.py
@@ -255,15 +255,15 @@ def search_windows(img, windows, clf, scaler, color_space='RGB',
 
 
 ### TODO: Tweak these parameters and see how the results change.
-color_space = 'HSV'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 9  # HOG orientations
+color_space = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 18  # HOG orientations
 pix_per_cell = 8  # HOG pixels per cell
 cell_per_block = 2  # HOG cells per block
 hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
 spatial_size = (64, 64)  # Spatial binning dimensions
 hist_bins = 64  # Number of histogram bins
 spatial_feat = False  # Spatial features on or off
-hist_feat = True  # Histogram features on or off
+hist_feat = False  # Histogram features on or off
 hog_feat = True  # HOG features on or off
 
 
